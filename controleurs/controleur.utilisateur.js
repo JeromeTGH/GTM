5d8922b4fa8f5da3b7fabe1f4ea1b68cc7fb5639
JeromeTGH <jerome.tomski@gmx.fr>
2023-02-23 10:03:14 +0100

    Reprise complète du code du controleur utilisateur, en utilisant async/await

--- controleurs/controleur.utilisateur.js
+++ controleurs/controleur.utilisateur.js
@@ -5,49 +5,53 @@ const ObjectID = require('mongoose').Types.ObjectId
 module.exports.getAll = async (req, res) => {
     try {
         const utilisateurs = await ModeleUtilisateur.find()
-        // dans les données retournées, manquents les password et email (données "sensibles") ; celles-ci sont masquées par défaut, par choix, dans le modèle
+        // dans les données retournées manquent les password et email (données "sensibles") ; celles-ci sont masquées par défaut, par choix, dans le modèle
         res.status(200).json(utilisateurs)
     }
     catch (err) {
-        res.status(500).json({ err })
+        console.log(err)
+        res.status(500).json(err)
     }
 }
 
 // Ajout fonction getOne dans API
-module.exports.getOne = (req, res) => {
+module.exports.getOne = async (req, res) => {
     if (!ObjectID.isValid(req.params.userID))
         return res.status(400).send(`ID [${req.params.userID}] inconnu …`)
 
-    ModeleUtilisateur.findById(req.params.userID, (err, data) => {
-        if (!err) res.status(200).json(data)
-        if (err) res.status(500).json({ err })
-    })
+    try {
+        const utilisateur = await ModeleUtilisateur.findById(req.params.userID)
+        res.status(200).json(utilisateur)
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
 }
 
 
 // Ajout fonction updateOne dans API
 module.exports.updateOne = async (req, res) => {
     if (!ObjectID.isValid(req.params.userID))
-        return res.status(400).send(`ID [${req.params.userID}] inconnu …`)
+        return res.status(400).json({ "err": `ID [${req.params.userID}] inconnu …` })
 
     if (req.body.pseudo === undefined && req.body.estActif === undefined)
-        return res.status(400).send(`Paramètre "pseudo" et/ou "estActif" manquant …`)
+        return res.status(400).json({ "err": `Paramètre "pseudo" et/ou "estActif" manquant …` })
+
+    const champsAmettreAjour = {}
+    if (req.body.pseudo !== undefined)
+        champsAmettreAjour['pseudo'] = req.body.pseudo
+    if (req.body.estActif !== undefined)
+        champsAmettreAjour['estActif'] = req.body.estActif
 
     try {
-        const filtre = { _id: req.params.userID }
-        const champsAmettreAjour = {}
-
-        if (req.body.pseudo !== undefined)
-            champsAmettreAjour['pseudo'] = req.body.pseudo
-        if (req.body.estActif !== undefined)
-            champsAmettreAjour['estActif'] = req.body.estActif
-
-        await ModeleUtilisateur.findOneAndUpdate(filtre, champsAmettreAjour, { new: true })     // Pour retourner l'élément APRÈS avoir fait l'update
-            .then((data) => res.status(200).json(data))
-            .catch((err) => res.status(500).json({ err }))
+        const utilisateurMisAjour = await ModeleUtilisateur.findByIdAndUpdate(req.params.userID, champsAmettreAjour, { new: true })
+        // {new: true} : pour retourner l'élément APRÈS avoir fait l'update
+        res.status(200).json(utilisateurMisAjour)
     }
     catch (err) {
-        res.status(500).json({ err })
+        console.log(err)
+        res.status(500).json(err)
     }
 }
 
@@ -57,13 +61,13 @@ module.exports.deleteOne = async (req, res) => {
         return res.status(400).send(`ID [${req.params.userID}] inconnu …`)
 
     try {
-        const filtre = { _id: req.params.userID }
-
-        await ModeleUtilisateur.deleteOne(filtre)
-            .then((data) => res.status(200).json(data))
-            .catch((err) => res.status(500).json({ err }))
+        const utilisateur = await ModeleUtilisateur.findByIdAndRemove(req.params.userID)
+        // Le contenu de l'utilisateur est retourné cette fois encore, mais il est désormais bien supprimé en base
+        // (si on réappelle cette fonction API, la valeur null sera alors retournée, prouvant que l'utilisateur a bien été supprimé)
+        res.status(200).json(utilisateur)
     }
     catch (err) {
-        res.status(500).json({ err })
+        console.log(err)
+        res.status(500).json(err)
     }
 }
