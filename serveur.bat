05ef1a1d08d89cf299232c8be393a5e3c28f2839
JeromeTGH <jerome.tomski@gmx.fr>
2023-02-24 13:04:31 +0100

    Refonte du fichier serveur.bat

--- serveur.bat
+++ serveur.bat
@@ -1,1 +1,13 @@
-node serveur.js
+@echo off
+cls
+echo. 
+echo ==================================================
+echo Lancement du serveur faisant tourner l'API en back
+echo      (faire double CTRL+C, pour quitter)
+echo ==================================================
+
+::Permet d'exécuter la commande "nodemon serveur.js", dans le cas de ce projet
+npm start
+
+:: Fin
+pause
